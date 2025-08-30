1144c8cea00018ca64216c3465794373901da486
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-08-30 12:55:54 +0530

    finished

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,64 @@
 const hre = require("hardhat");
 
 async function main() {
+   const accounts = await ethers.getSigners();
+    const buyer = accounts[0];      // Account #0
+    console.log("buyer: ",buyer.address);
+  const seller = accounts[1];     // Account #1
+  console.log("seller: ",seller.address);
+  const inspector = accounts[3];  // Account #3
+  console.log("inspector: ",inspector.address)
+  const lender = accounts[4]; 
+  console.log("lender: ",lender.address)
+
+  const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+  }
+
+    // Deploy Real Estate
+    const RealEstate = await ethers.getContractFactory('RealEstate')
+    realEstate = await RealEstate.deploy()
+    await realEstate.deployed()
+    console.log(`Deployed Real Estate Contract at: ${realEstate.address}`);
+    console.log(`Minting 3 properties... \n`)
+    
+    for ( let i = 1; i<= 3 ; i++){
+      const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i}.json`);
+      await transaction.wait()
+      console.log("txn id:",transaction.hash);
+    }
+
+    const Escrow = await ethers.getContractFactory('Escrow')
+    const escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    )
+    await escrow.deployed();
+
+
+  console.log(`Deployed Escrow Contract at: ${escrow.address}`)
+  console.log(`Listing 3 properties...\n`)
+
+  for (let i = 0; i < 3; i++) {
+    // Approve properties...
+    let transaction = await realEstate.connect(seller).approve(escrow.address, i + 1)
+    await transaction.wait()
+  }
+
+  // Listing properties...
+  transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
+  await transaction.wait()
+
+  console.log(`Finished.`)
+
 
 }
 
